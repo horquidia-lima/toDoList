71c735129fe27bc62b2aa3e16342b98cb4382cad
horquidia-lima <horquidia.a.lima@gmail.com>
2024-04-23 20:04:02 -0300

    css task created

--- ToDoList-js/ToDoList/src/components/TasksCreated.jsx
+++ ToDoList-js/ToDoList/src/components/TasksCreated.jsx
@@ -1,3 +1,4 @@
+import {Trash} from 'phosphor-react'
 import styles from './TasksCreated.module.css'
 
 export function TasksCreated(){
@@ -14,6 +15,33 @@ export function TasksCreated(){
                     <span> 2 de 5</span>
                 </p>
             </div>
+
+            <div className={styles.tareas}>
+                <div className={styles.tarea}> 
+                    <input type="checkbox" /> 
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    <Trash size={18} />
+                </div>
+
+                <div className={styles.tarea}> 
+                    <input type="checkbox" /> 
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    <Trash size={18} />
+                </div>
+
+                <div className={styles.tarea}> 
+                    <input type="checkbox" /> 
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    <Trash size={18} />
+                </div>
+
+                <div className={styles.tarea}> 
+                    <input type="checkbox" /> 
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                    <Trash size={18} />
+                </div>
+            </div>
+            
             
         </div>
     )
